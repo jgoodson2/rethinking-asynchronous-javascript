1b3ffeed7caf81ef456e0e58cb0d2218608d5fbe
jgoodson2 <jonathonGoodson@clayton.edu>
2017-05-24 17:06:24 -0400

    working on ex2 solution

--- ex2/ex2.js
+++ ex2/ex2.js
@@ -1,42 +1,52 @@
-function fakeAjax(url,cb) {
-	var fake_responses = {
-		"file1": "The first text",
-		"file2": "The middle text",
-		"file3": "The last text"
-	};
-	var randomDelay = (Math.round(Math.random() * 1E4) % 8000) + 1000;
+function fakeAjax(url, cb) {
+  var fake_responses = {
+    "file1": "The first text",
+    "file2": "The middle text",
+    "file3": "The last text"
+  };
+  var randomDelay = (Math.round(Math.random() * 1E4) % 8000) + 1000;
 
-	console.log("Requesting: " + url);
+  console.log("Requesting: " + url);
 
-	setTimeout(function(){
-		cb(fake_responses[url]);
-	},randomDelay);
+  setTimeout(function () {
+    cb(fake_responses[url]);
+  }, randomDelay);
 }
 
 function output(text) {
-	console.log(text);
+  console.log(text);
 }
 
 // **************************************
 
 function getFile(file) {
-	// what do we do here?
 
+  var text;
+
+  fakeAjax(file, function (response) {
+    text = response
+    // console.log(response)
+    // return response
+  })
+
+  return function (cb) {
+    cb(text)
+  }
 }
 
 var th1 = getFile("file1")
 var th2 = getFile("file2")
 var th3 = getFile("file3")
 
-th1(function(text1){
-	output(text1)
-	th2(function(text2){
-		output(text2)
-		th1(function(text1){
-			output(text1)
-			output("Complete!")
-		})
-	})
+th1(function (text1) {
+  output(text1)
+  th2(function (text2) {
+    output(text2)
+    th3(function (text3) {
+      output(text3)
+      output("Complete!")
+    })
+  })
 })
 
 
