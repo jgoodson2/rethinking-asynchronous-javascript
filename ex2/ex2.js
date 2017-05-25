10272ec93ba3812a98c69bb0780ee7ddee946292
jonathongoodson <goodson.jonathon@gmail.com>
2017-05-25 16:56:54 -0400

    Restored ex2.js to original form

--- ex2/ex2.js
+++ ex2/ex2.js
@@ -1,59 +1,27 @@
-function fakeAjax(url, cb) {
-  var fake_responses = {
-    "file1": "The first text",
-    "file2": "The middle text",
-    "file3": "The last text"
-  };
-  var randomDelay = (Math.round(Math.random() * 1E4) % 8000) + 1000;
+function fakeAjax(url,cb) {
+	var fake_responses = {
+		"file1": "The first text",
+		"file2": "The middle text",
+		"file3": "The last text"
+	};
+	var randomDelay = (Math.round(Math.random() * 1E4) % 8000) + 1000;
 
-  console.log("Requesting: " + url);
+	console.log("Requesting: " + url);
 
-  setTimeout(function () {
-    cb(fake_responses[url]);
-  }, randomDelay);
+	setTimeout(function(){
+		cb(fake_responses[url]);
+	},randomDelay);
 }
 
 function output(text) {
-  console.log(text);
+	console.log(text);
 }
 
 // **************************************
 
 function getFile(file) {
-
-  var text;
-
-  fakeAjax(file, function (response) {
-    text = response
-    // console.log(response)
-    // return response
-  })
-
-  return function (cb) {
-    cb(text)
-  }
+	// what do we do here?
 }
 
-var th1 = getFile("file1")
-var th2 = getFile("file2")
-var th3 = getFile("file3")
-
-th1(function (text1) {
-  output(text1)
-  th2(function (text2) {
-    output(text2)
-    th3(function (text3) {
-      output(text3)
-      output("Complete!")
-    })
-  })
-})
-
-
-
-
-
-
-
-
-
+// request all files at once in "parallel"
+// ???
