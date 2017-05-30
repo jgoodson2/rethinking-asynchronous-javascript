b817d9db9f573bf6ad5e1531b0be29b93fcedadd
jgoodson2 <jonathonGoodson@clayton.edu>
2017-05-30 08:34:57 -0400

    Incremental

--- ex3/ex3.js
+++ ex3/ex3.js
@@ -1,27 +1,50 @@
-function fakeAjax(url,cb) {
-	var fake_responses = {
-		"file1": "The first text",
-		"file2": "The middle text",
-		"file3": "The last text"
-	};
-	var randomDelay = (Math.round(Math.random() * 1E4) % 8000) + 1000;
+function fakeAjax(url, cb) {
+  var fake_responses = {
+    "file1": "The first text",
+    "file2": "The middle text",
+    "file3": "The last text"
+  };
+  var randomDelay = (Math.round(Math.random() * 1E4) % 8000) + 1000;
 
-	console.log("Requesting: " + url);
+  console.log("Requesting: " + url);
 
-	setTimeout(function(){
-		cb(fake_responses[url]);
-	},randomDelay);
+  setTimeout(function () {
+    cb(fake_responses[url]);
+  }, randomDelay);
 }
 
 function output(text) {
-	console.log(text);
+  console.log(text);
 }
 
 // **************************************
 
 function getFile(file) {
-	// what do we do here?
+  // what do we do here?
+  return new Promise(function executor(resolve, reject) {
+    fakeAjax(file, resolve)
+  })
 }
+
 
 // request all files at once in "parallel"
 // ???
+
+var p1 = getFile("file1")
+var p2 = getFile("file2")
+var p3 = getFile("file3")
+
+p1
+  .then(output)
+  .then(function () {
+    return p2
+  })
+  .then(output)
+  .then(function () {
+    return p3
+  })
+  .then(output)
+  .then(function () {
+      output("Complete!")
+    }
+  )
